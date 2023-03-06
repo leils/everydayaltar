24d51798831bc0e487a4901a2ae61820de7f27f8
leils <e.leiachang@gmail.com>
2023-03-06 01:10:41 -0500

    stashed updates

--- tester_code/str_util.py
+++ tester_code/str_util.py
@@ -1,5 +1,7 @@
 import textwrap
 
+# taken from https://forums.adafruit.com/viewtopic.php?f=19&t=56504
+
 def textWrapped(text, maxColumn):         #maxColumn can be fetched from Adafruit_Thermal.py (it is 32)
 	textWrapped = textwrap.wrap(text, width=maxColumn)
 	for i in range(len(textWrapped)):
