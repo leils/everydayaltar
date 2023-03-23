d763ae2e337c4d1ed36afe5f683a239d908aab86
Leia <e.leiachang@gmail.com>
2023-03-23 10:46:52 -0400

    adding usb

--- tester_code/print_multiple.py
+++ tester_code/print_multiple.py
@@ -4,7 +4,8 @@ import str_util
 from pathlib import Path
 
 ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.68)
-uart = serial.Serial("/dev/serial1", baudrate=19200, timeout=3000)
+# uart = serial.Serial("/dev/serial1", baudrate=19200, timeout=3000)
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
 
 dirname = os.path.dirname(__file__)
 p = Path(dirname)
