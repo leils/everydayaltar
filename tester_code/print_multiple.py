b634090402db8c5eb5b8c1be3c2506b53fad32c9
Leia <e.leiachang@gmail.com>
2023-03-23 10:45:18 -0400

    oh

--- tester_code/print_multiple.py
+++ tester_code/print_multiple.py
@@ -1,19 +1,32 @@
-import serial
+import serial, os
 import adafruit_thermal_printer
 import str_util
+from pathlib import Path
 
 ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.68)
 uart = serial.Serial("/dev/serial1", baudrate=19200, timeout=3000)
 
+dirname = os.path.dirname(__file__)
+p = Path(dirname)
+parentPath = str(p.parent)
+source_filename = os.path.join(parentPath, 'shared/source.json')
+datastore = os.path.join(parentPath, "shared/data.json")
+
+f = open(datastore)
+raw_data = json.load(f)
+
 printer = ThermalPrinter(uart)
 
+# printer.test_page()
 printer.feed(2)
+#printer.bold = True
+# printer.inverse = True
 printer.up_down_mode = True
 
-
-while(1):
-    i = input()
-    s = str_util.textWrapped(i, 32)
-    printer.print(s)
+for q, a in raw_data.items():
+    printer.print(str_util.textWrapped(q, 32))
     printer.feed(2)
+    for answer in a: 
+        printer.print(str_util.textWrapped(a, 32))
+
 
