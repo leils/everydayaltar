c0a425c912a660bf911ecf7a37675e2df4bfefaf
Leia <e.leiachang@gmail.com>
2023-03-25 12:44:23 -0400

    retrieving responses 01

--- tester_code/matching_test.py
+++ tester_code/matching_test.py
@@ -11,8 +11,19 @@ from shared import utils
 ### FILE SETUP 
 sharedPath = path + '/shared'
 source_filename = os.path.join(sharedPath, 'source.json')
-datastore = os.path.join(sharedPath, "data.json")
+data_filename = os.path.join(sharedPath, "data.json")
 
-f = open(source_filename)
-raw_data = json.load(f)
-questions = raw_data['questions']
+questions = json.load(open(source_filename))['questions']
+responses = json.load(open(data_filename))
+
+while(1): 
+    print('-------------------------------')
+    input('press enter to retrieve')
+    q = random.choice(questions)
+    print(q)
+    print('responses:')
+    if q in responses: 
+        print(responses[q])
+    else: 
+        print("not found")
+
