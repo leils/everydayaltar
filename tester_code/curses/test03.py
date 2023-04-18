fd8c083f583a3fc144065940fc0ecd57821e8370
Leia <e.leiachang@gmail.com>
2023-04-17 20:57:42 -0400

    test3 working

--- tester_code/curses/test03.py
+++ tester_code/curses/test03.py
@@ -1,6 +1,6 @@
 # Test to get curses and printout to work back and forth
-import curses, traceback, time
-from curses.textpad import Textbox
+import curses, os
+from curses.textpad import Textbox, rectangle
 def validate(ch):
 
     # exit input with the escape key
@@ -20,9 +20,11 @@ def validate(ch):
 
 def getInput(stdscr):
     stdscr.clear()
-    stdscr.addstr(0, 0, "write a message please")
+    stdscr.addstr(0, 0, "enter a message")
 
     editwin = curses.newwin(10,30, 2,1)
+    rectangle(stdscr, 1, 0, 12, 32) # apparently rectangle needs to be 2 larger than the input window
+    stdscr.refresh()
     # editwin.box()
 
     editBox = Textbox(editwin, insert_mode=True)
@@ -33,9 +35,12 @@ def getInput(stdscr):
 
 
 def main(): 
+    os.system('clear')
     print('hello, this is waiting for something')
     input()
     inputString = curses.wrapper(getInput)
+
+    os.system('clear')
     print(inputString)
     input()
     
