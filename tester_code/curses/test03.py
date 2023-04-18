5dcd5ce1bcd827bcadada195ce9ac6756a9ce179
Leia <e.leiachang@gmail.com>
2023-04-17 21:18:07 -0400

    creating rectangles

--- tester_code/curses/test03.py
+++ tester_code/curses/test03.py
@@ -1,5 +1,5 @@
 # Test to get curses and printout to work back and forth
-import curses, os
+import curses, os, time
 from curses.textpad import Textbox, rectangle
 def validate(ch):
 
@@ -22,10 +22,18 @@ def getInput(stdscr):
     stdscr.clear()
     stdscr.addstr(0, 0, "enter a message")
 
-    editwin = curses.newwin(10,30, 2,1)
-    rectangle(stdscr, 1, 0, 12, 32) # apparently rectangle needs to be 2 larger than the input window
+    rows, cols = stdscr.getmaxyx()
+    # stdscr.addstr(2, 0, str(rows))
+    # stdscr.addstr(4, 0, str(cols))
+    stdscr.refresh()
+
+    # time.sleep(20)
+
+    editwin = curses.newwin(rows-4, cols-2, 2,1)
+    rectangle(stdscr, 1, 0, rows-2, cols-1) # apparently rectangle needs to be 2 larger than the input window
     stdscr.refresh()
     # editwin.box()
+    # time.sleep(20)
 
     editBox = Textbox(editwin, insert_mode=True)
     editBox.edit(validate)
@@ -36,7 +44,7 @@ def getInput(stdscr):
 
 def main(): 
     os.system('clear')
-    print('hello, this is waiting for something')
+    print('hello, this is waiting for enter')
     input()
     inputString = curses.wrapper(getInput)
 
